84b4ea3c57e8fd46351a28099a3863b78807891c
Robert Irving <randrewirving@gmail.com>
2018-11-27 15:59:54 -0700

    completed, including black diamon

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
+import History from './../History/History';
+import Contact from './../Contact/Contact';
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+         <Link to='/about' className='subnav_links'><h3>About</h3></Link>
+         <Link to='/about/history' className='subnav_links'> <h3>History</h3></Link>
+         <Link to='/about/contact' className='subnav_links'><h3>Contact</h3></Link>
         </div>
         <div className='box'>
+          <Switch>
+            <Route path='/about/history' component={History}/>
+            <Route path='/about/contact' component={Contact}/>
+            <Route path='/about' render={()=>(
+              <h1>This University is amazing!</h1>
+            )}/>
+          </Switch>
         </div>
       </div>
     )
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+// import {Link} from 'react-router-dom';
 export default class Student extends Component {
   constructor() {
-    super()
-
+    super();
+    this.goBack = this.goBack.bind(this);
+    
+    this.state = {
+      studentInfo: {}
+    };
   }
-
-  render() {
+  goBack(){
+    this.props.history.goBack();
+}
+  componentDidMount() {
+    return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then( results => {
+      this.setState({
+        studentInfo: results.data
+      });
+    });
+  }
+    
+  render() 
+  {
     return (
-      <div className="box">
-        <h1>Student</h1>
+      <div className='box'>
+      <button onClick={this.goBack}>Back</button>
+      {/* {console.log(this.props.history)} */}
+        <h1>Student:</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
       </div>
     )
   }
 }
+
